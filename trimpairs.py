13d62da9a84fefe4b4ca8caefd2173cc502fa936
Colin Pillsbury <cpillsb1@swarthmore.edu>
2017-05-08 20:42:42 -0400

    added scraper that reads the pairs.json.txt file and the coordinate tsv file and writes a json file

---
+++ trimpairs.py
@@ -0,0 +1,64 @@
+"""
+Python script used to filter out the pairs in pairs.json.txt that do not contain
+languages with coordinates in the tsv file
+"""
+
+def filterPairs(filename, languages):
+    pairFile = open(filename, "r")
+    filtered = []
+    count = 1
+    for line in pairFile:
+        line = line.strip().split()
+        lang1 = line[9][1:4]
+        lang2 = line[11][1:4]
+        if lang1 in languages and lang2 in languages:
+            coordString1 = '"coordinates1": ' + str(languages[lang1]) + ","
+            coordString2 = '"coordinates2": ' + str(languages[lang2]) + ","
+            print(coordString2)
+            line.insert(12, coordString1)
+            line.insert(13, coordString2)
+            line[-1] = line[-1][:-1]
+            joined = " ".join(line)
+            filtered.append(joined)
+
+    pairFile.close()
+    return filtered
+
+def getLanguagesWithCoords(filename):
+
+    langFile = open(filename, "r")
+    langDict = {}
+    for line in langFile:
+        line = line.strip().split(",")
+        lat, lon = float(line[1]), float(line[2])
+        langDict[line[0]] = [lon, lat]
+    langFile.close()
+    return langDict
+
+if __name__ == "__main__":
+    print("Ok")
+    langDict = getLanguagesWithCoords("apertium-languages.tsv")
+    print(langDict)
+    filtered = filterPairs("pairs.json.txt", langDict)
+    print(filtered[0])
+    apertiumFile = open("apertiumPairs.json", "w")
+
+    apertiumFile.write("{\n")
+    apertiumFile.write('"type": "FeatureCollection",\n')
+    apertiumFile.write('"pairs": [\n')
+    for line in filtered:
+        apertiumFile.write("\t"+line+"\n")
+        apertiumFile.write(",\n")
+    apertiumFile.write("]\n")
+    apertiumFile.write(",\n")
+    #writing point coordinates
+    apertiumFile.write('"point_data": [\n')
+    for code, coords in langDict.items():
+        string = '{"tag": "' + code + '", ' + '"coordinates": ' + str(coords) + "}\n"
+        apertiumFile.write("\t"+string)
+        apertiumFile.write(",\n")
+    apertiumFile.write("]\n")
+    apertiumFile.write("}")
+
+    apertiumFile.close()
+    # print " ".join(filtered[0])
